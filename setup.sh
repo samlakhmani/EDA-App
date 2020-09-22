526866169c8431cf916f05d14817c476c5fcec65
Samriddh <samriddhlakhmani@gmail.com>
2020-09-22 23:33:27 +0530

    IDK

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “
-[server]
-headless = true
-enableCORS=false
-port = $PORT
-“ > ~/.streamlit/config.toml
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
