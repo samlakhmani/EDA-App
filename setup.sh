89da4962a869a057d05c48f2183ce88e1b44ffd0
Samriddh <samriddhlakhmani@gmail.com>
2020-09-22 23:25:44 +0530

    123

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"samriddhlakhmani@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo “
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+“ > ~/.streamlit/config.toml
